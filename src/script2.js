17eb8e5c1e4e03437d5fd2e8ced291cc57ed15a5
farsilvar <56745044+farsilvar@users.noreply.github.com>
2021-11-28 17:48:59 -0300

    Add files via upload

--- src/script2.js
+++ src/script2.js
@@ -4,45 +4,28 @@ function getData() {
 	var url = `https://newsapi.org/v2/everything?q=${x}&apiKey=${apiKey}`
 	fetch(url).then((res)=>{
    		return res.json()
-   		}).then((data)=>{
-	   		console.log(data)
-			var i  = 0;
-			var textoResultados = '';
-			for (i = 0; i < data.articles.length; i++) {
-				var newsTitle = data.articles[i].title
-				if (newsTitle == null) {
-					let description = data.articles[i].description;
-					let ptIndex = description.indexOf(".");
-					let descriptionPt = description.slice(0, ptIndex);
-					newsTitle = descriptionPt;
-				}
-				let newsTitle = data.articles[i].title
-				if (newsTitle == null) {
-					let description = data.articles[i].description;
-					let ptIndex = description.indexOf(".");
-					let descriptionPt = description.slice(0, ptIndex);
-					newsTitle = descriptionPt;
-				}
-					textoResultados += 
-					`<fieldset class="field-result">
-							<a href=${data.articles[i].url}><img src=${data.articles[i].urlToImage} style="float: left;" width="230" height="230"></a>
-							<h3 style="padding-right: 300px;">${newsTitle}</h3>
-							<details style="padding-right: 500px;">
-							<p style="padding-right: 20px;">${data.articles[i].description}</p>
-							<strong><p>Clique na imagem para a notícia completa</p></strong>
-							</div>
-					</fieldset>`
-				};
-				document.querySelector(".b").innerHTML = textoResultados;
-				//validateForm(data)
-			})
-			//req.onload = getData();
-			//req.open('GET', url, true);
-			//req.send();
-		}
-		function openWin() {
-			var w = window.open();
-  			w.document.open();
-  			w.document.write("<h2>Hello World!</h2>");
-  			w.document.close();
-		}
+	}).then((data)=>{
+   		console.log(data)
+		var i  = 0;
+		var textoResultados = '';
+		for (i = 0; i < data.articles.length; i++) {
+            var newsTitle = data.articles[i].title
+			if (newsTitle == null) {
+				let description = data.articles[i].description;
+				let ptIndex = description.indexOf(".");
+				let descriptionPt = description.slice(0, ptIndex);
+				newsTitle = descriptionPt;
+			}
+			textoResultados += 
+				`<fieldset class="field-1">
+					<a href=${data.articles[i].url}><img src=${data.articles[i].urlToImage} style="float: left;" width="230" height="230"></a>
+					<h3 style="padding-right: 300px;">${newsTitle}</h3>
+					<details style="padding-right: 500px;">
+						<p style="padding-right: 20px;">${data.articles[i].description}</p>
+						<strong><p>Clique na imagem para a notícia completa</p></strong>
+					</div>
+				</fieldset>`
+		};
+		document.querySelector(".b").innerHTML = textoResultados;
+	})
+}
